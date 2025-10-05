2ba055dca14d09ff0d1a950f015e2c65a0ab6ee4
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2025-10-05 11:16:13 +0530

    learned getElementsByTagName

---
+++ 002 DOM Manipulation/005 getElementsByTagName.js
@@ -0,0 +1,8 @@
+function magic() {
+  let b1 = Array.from(document.getElementsByName("div"));
+  // console.log(b1)
+  for (Element of div) {
+    Element.classlist.add("black");
+    Element.classlist.remove("black");
+  }
+}
